c577e6dee7ce254cade8dff5cd7ae8848ac134fc
Cin1983! <cinnymaria@gmail.com>
2021-02-01 19:56:47 +0100

    FooterTest flex 1 februari 2021

---
+++ src/footer/container/footerTest.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function footerTest() {
+  return (
+    <div className="footerStyle">
+      <div className="column_1">
+        <link to="#">Investor Relations </link>
+        <link to="#">Ways to Watch</link>
+        <link to="#">BingeWatch originals</link>
+        <link to="#">Team </link>
+      </div>
+
+      <div className="column_2">
+        <link to="#">Help Centre</link>
+        <link to="#">Jobs</link>
+        <link to="#">Terms of Use</link>
+      </div>
+
+      <div className="column_3">
+        <link to="#">Account</link>
+        <link to="#">Redeem Gift Cards</link>
+        <link to="#">Privacy</link>
+      </div>
+
+      <div className="column_4">
+        <link to="#">Media Centre</link>
+        <link to="#">Buy Gift Cards</link>
+        <link to="#">Cookie Preferences</link>
+      </div>
+    </div>
+  );
+}
+
+export default footerTest;
