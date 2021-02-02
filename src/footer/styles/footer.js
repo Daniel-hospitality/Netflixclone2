9affadbc51c3f8a38cdaca3679c3a85042700762
Halil Bagci <halilbagci75@hotmail.com>
2021-02-02 12:11:20 +0100

    Update footer to make it and icon links responsive

--- src/footer/styles/footer.js
+++ src/footer/styles/footer.js
@@ -3,106 +3,81 @@ import styled from "styled-components/macro";
 export const Container = styled.div`
   display: flex;
   padding: 70px 56px;
+  ${'' /* padding-bottom: 20px; */}
   margin-left: auto;
-  max-width: 1080px;
+  max-width: 100%;
   flex-direction: column;
 `;
-
-
 
 export const Row = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
-  grid-gap: 15px;
+  grid-template-columns: repeat(2, minmax(100px, 1fr));
+  ${"" /* grid-template-column: max-content; */}
+  ${'' /* grid-gap: 15px; */}
   padding-top: 15px;
-  margin-left: 19%;
+  padding-bottom: 50px;
+  justify-content: space-between;
+  margin-left: 10%;
+  margin-right: 10%;
 
- 
-
-  @media only screen and (max-width: 767px), screen and (min-width: 576px) {
-    margin-left: 19%;
-    font-size: 150%;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(140px, 2fr));
-    grid-gap: 10px;
-    margin-left:5%;
-    padding-right:50px; 
+  @media screen and (min-width: 567px) {
+    grid-template-columns: repeat(2, minmax(100px, 1fr));
+    ${'' /* grid-gap: 10px; */}
+    
   }
 
-  @media only screen and (max-width: 991px), screen and (min-width:768px) {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 2fr));
-    margin-left:5%;
-
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(4, minmax(50px, 1fr));
+   
   }
 
-  @media only screen and (max-width: 1199px), screen and (min-width:992px) {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 2fr));
-    margin-left:5%;
+  @media screen and (min-width: 992px) {
+    grid-template-columns: repeat(4, minmax(50px, 1fr));
   }
 
-  @media only screen and  (min-width: 1200px ) {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 2fr));
-    margin-left:5%;
+  @media only screen and (min-width: 1200px) {
+    grid-template-columns: repeat(4, minmax(200px, 1fr));
+    justify-content: space-between;
+    margin-left: 15%;
+    margin-right: 15%;
   }
 `;
 
-
 export const Column = styled.div`
-  display: flex;
+  display: grid;
   justify-content: space-between;
   text-align: left;
-  margin-left: 65px;
+  ${"" /* margin-left: 65px; */}
   max-width: 1000px;
   flex-direction: column;
 
-  @media (max-width: 1080px) {
-    padding: 21px;
-  }
-
   @media only screen and (max-width: 767px), screen and (min-width: 576px) {
-    display: grid;
     grid-template-columns: repeat(auto-fit, minmax(320%, 1fr));
-    grid-gap: 15px;
-    margin-left: 15%;
+  }
 
-    @media only screen and (max-width: 991px), screen and (min-width:768px ;) {
-    display: grid;
+  @media only screen and (max-width: 991px), screen and (min-width: 768px) {
     grid-template-columns: repeat(auto-fit, minmax(350%, 1fr));
-    grid-gap: 15px;
-    margin-left: 15%;
-    width:50%;
   }
 
-
-
-
-
-  @media only screen and (max-width: 1199px), screen and (min-width:992px ;) {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(100px, 2fr));
-    margin-left:-10%;
+  @media only screen and (max-width: 1199px), screen and (min-width: 992px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   }
 
-  @media only screen and  (min-width: 1200px ) {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 2fr));
-    margin-left:5%;
+  @media only screen and (min-width: 1200px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   }
-
-
 `;
 
 export const Link = styled.a`
   display: flex;
   color: #757575;
-  margin-bottom: 20px;
+  ${"" /* margin-bottom: 3px; */}
   font-size: 15px;
   text-decoration: none;
-  @media only screen and (max-width: 767px), screen and (min-width: 576px) {
+  ${
+    "" /* @media only screen and (max-width: 767px), screen and (min-width: 576px) {
    margin-bottom:0px;
+  } */
   }
 `;
 
@@ -110,31 +85,31 @@ export const Icon = styled.i`
   display: flex;
   justify-content: space-between;
   font-size: 27px;
-  margin-left: 22.4%;
-  width: 9%;
+  margin-left: 10%;
+  margin-right: 10px;
+  width: 30%;
   color: silver;
+  padding-left: 0;
 
-
-  @media only screen and (max-width: 767px), screen and (min-width: 576px) {
-    margin-left: 14%;
-    font-size: 150%;
-    width: 25%;
+  
+  @media screen and (min-width: 576px) {
+       width: 20%;
+    
   }
 
-  @media only screen and (max-width: 991px), screen and (min-width:768px ;) {
-    margin-left: 13%;
+  @media screen and (min-width: 768px ;) {
+    width: 20%;
   }
 
-  @media only screen and (max-width: 1199px), screen and (min-width:992px ;) {
-    margin-left: 5%;
+  @media screen and (min-width: 992px ;) {
+  
+    width: 15%;
   }
 
-  @media only screen and  (min-width: 1200px ) {
-    margin-left:5%;
-    width:16%;
+  @media only screen and (min-width: 1200px) {
+    margin-left: 15%;
+    width: 10%;
   }
-
-
 `;
 
 export const Link1 = styled.a`
