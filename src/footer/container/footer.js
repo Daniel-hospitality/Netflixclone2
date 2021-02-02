7e7e5b9a9efa1f538a7b60b3e19717b3e1dd8e7a
Halil Bagci <halilbagci75@hotmail.com>
2021-02-02 12:10:22 +0100

    Delete team link to align footer and delete footer Breaks

--- src/footer/container/footer.js
+++ src/footer/container/footer.js
@@ -34,28 +34,28 @@ export function FooterContainer() {
           <Footer.Link href="#">Investor Relations</Footer.Link>
           <Footer.Link href="#">Ways to Watch</Footer.Link>
           <Footer.Link href="#">BingeWatch originals</Footer.Link>
-          <Footer.Link href="#">Team </Footer.Link>
+          {/* <Footer.Link href="#">Team </Footer.Link> */}
         </Footer.Column>
 
         <Footer.Column>
           <Footer.Link href="#">Help Centre</Footer.Link>
           <Footer.Link href="#">Jobs</Footer.Link>
           <Footer.Link href="#">Terms of Use</Footer.Link>
-          <Footer.Break></Footer.Break>
+          {/* <Footer.Break></Footer.Break> */}
         </Footer.Column>
 
         <Footer.Column>
           <Footer.Link href="#">Account</Footer.Link>
           <Footer.Link href="#">Redeem Gift Cards</Footer.Link>
           <Footer.Link href="#">Privacy</Footer.Link>
-          <Footer.Break></Footer.Break>
+          {/* <Footer.Break></Footer.Break> */}
         </Footer.Column>
 
         <Footer.Column>
           <Footer.Link href="#">Media Centre</Footer.Link>
           <Footer.Link href="#">Buy Gift Cards</Footer.Link>
           <Footer.Link href="#">Cookie Preferences</Footer.Link>
-          <Footer.Break></Footer.Break>
+          {/* <Footer.Break></Footer.Break> */}
         </Footer.Column>
       </Footer.Row>
     </footer>
