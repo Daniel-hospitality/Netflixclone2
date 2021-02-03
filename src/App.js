87437eebc72af84a2155129fbc1a17b762e72d36
Halil Bagci <halilbagci75@hotmail.com>
2021-02-03 15:12:00 +0100

    Update to deactivate hero component import

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Row from "./Row";
 import requests from "./requests";
-import Hero from "./components/Hero";
+// import Hero from "./components/Hero";
 import Backend from "./Backend";
 // eigen import //
 // import { Series, Films, Nieuw, Lijst, Kijk } from "./pages";
