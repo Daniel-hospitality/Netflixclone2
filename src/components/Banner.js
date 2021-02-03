4cd5e1d2d5b93549d0080f70681253d41ae447f0
Halil Bagci <halilbagci75@hotmail.com>
2021-02-03 15:13:03 +0100

    Add movie fetch to banner and update props.movie to movie

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,21 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Player from "./Player";
-import InfoButton from "./MoreInfoButton/InfoButton"
+import InfoButton from "./MoreInfoButton/InfoButton";
+import Backend from "../Backend";
 import "./Banner.css";
+ 
 
-function Banner(props) {
+function Banner() {
+  const [movie, setMovie] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const response = await Backend.fetchMovies(122);
+      setMovie(response.data);
+    }
+    fetchData();
+  }, []);
   const player = useRef(null);
-
+  
   const handleClickFullscreen = () => {
     player.current.enableFullscreen();
   };
@@ -20,20 +30,20 @@ function Banner(props) {
           <i class="fas fa-volume-mute"></i>
         </button>
       <div className="player-wrapper">
-        <Player ref={player} movie={props.movie}/>
+        <Player ref={player} movie={movie}/>
       </div>
       <div className="banner_contents">
         <img
           className="banner_movielogo"
-          src={props.movie?.logoUrl}
-          alt={props.movie?.title + "logo"}
+          src={movie?.logoUrl}
+          alt={movie?.title + "logo"}
         /> 
-        <h1 className="banner_description">{props.movie?.overview}</h1>
+        <h1 className="banner_description">{movie?.overview}</h1>
         <div className="banner_buttons">
           <button className="banner_button" onClick={handleClickFullscreen}>
             Play
           </button>
-          <InfoButton movie={props.movie} />
+          <InfoButton movie={movie} />
         </div>
       </div>
       <div className="banner_fadeBottom" />
