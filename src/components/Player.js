664197b07644a633a6c180afb7cfcebec63176e0
Abraham Engel <engelenburg1980@gmail.com>
2021-02-04 08:42:04 +0100

    Add mute and exit button to Player component. Utilises useState, useEffect hook screenfull’s event listeners to toggle buttons on fullscreen changes.

--- src/components/Player.js
+++ src/components/Player.js
@@ -8,52 +8,93 @@ import React, {
 import ReactPlayer from "react-player";
 import screenfull from "screenfull";
 import "./Banner.css";
+import "./Player.css";
+import LineIcon from 'react-lineicons';
 
 function Player(props, ref) {
   const player = useRef(null);
+  const playerWrapper = useRef(null);
 
   const [playVideo, setPlayVideo] = useState(false);
   const [light, setLight] = useState(false);
   const [muted, setMuted] = useState(true);
+  const [hidden, setHidden] = useState('hidden');
 
   useImperativeHandle(ref, () => ({
     enableFullscreen: () => {
       if (screenfull.isEnabled) {
-        screenfull.request(player.current.wrapper);
-        setMuted(false);
-      }
+        screenfull.request(playerWrapper.current);
+      } 
     },
     toggleMute: () => {
-      if (muted) {
-        setMuted(false);
-      } else {
-        setMuted(true);
-      }
+      muted ? setMuted(false) : setMuted(true);
     }
   }));
-
+  
   useEffect(() => {
+    const toggleScreenfull = () => {
+      if (screenfull.isEnabled) {
+        screenfull.on('change', () => {
+          screenfull.isFullscreen ? setMuted(false) : setMuted(true);
+        });
+      }
+    };
+    // const handleMouseMove = (event) => {
+    //   if (screenfull.isFullscreen) {
+
+    //     });
+    //   }
+    // };
     setTimeout(() => {
       setPlayVideo(true);
       setLight(true);
     }, 5000);
+    toggleScreenfull();
   }, []);
 
+
+//   useEffect(() => {
+//     const handleEsc = (event) => {
+//         if (event.keyCode === 27) 
+//         setMuted(true);
+//     };
+//     window.addEventListener('keydown', handleEsc);
+//     return () => {
+//     window.removeEventListener('keydown', handleEsc);
+//     };
+// }, []);
+
+
+  function handleExit(){
+    screenfull.exit();
+  }
+
+  const handleMute = () => {
+    muted ? setMuted(false) : setMuted(true);
+  }
+
   return (
-    <div>
+    <div ref={playerWrapper}>
         <ReactPlayer
           className="banner-player"
           url={`https://youtu.be/${props.movie?.youtubeKey}`}
-          playing={playVideo}
           width="100%"
           height="100%"
           ref={player}
+          playing={playVideo}
+          muted={muted}
           playbackRate={1}
           controls={false}
-          muted={muted}
           loop={true}
           light={`${light ? "" : `https://image.tmdb.org/t/p/original${props.movie?.backdropPath}`}`}
         />
+        <button className={hidden} id="player-fs-exit-btn" onClick={handleExit}>
+          <LineIcon name="close"/>
+        </button>
+
+        <button className={hidden} id="player-fs-mute-btn" onClick={handleMute}>
+          <LineIcon name="volume-mute"/>
+        </button>
     </div>
   );
 }
