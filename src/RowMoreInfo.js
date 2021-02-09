bb92cd142d594d200ecb4e723a305eede1d78a7f
Cin1983! <cinnymaria@gmail.com>
2021-02-09 11:29:02 +0100

    Updat RowMoreInfo

---
+++ src/RowMoreInfo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function RowMoreInfo() {
+
+    const onMouseOver{
+        
+    }
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RowMoreInfo
