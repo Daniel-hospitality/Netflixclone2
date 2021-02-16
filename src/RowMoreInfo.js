638a557946cfb02e83ca36d079fb9da83deab516
Cin1983! <cinnymaria@gmail.com>
2021-02-16 13:04:59 +0100

    Update detailcart

--- src/RowMoreInfo.js
+++ src/RowMoreInfo.js
@@ -8,29 +8,29 @@ function RowMoreInfo(props) {
   const [muted, setMuted] = useState(true);
 
   const toggleMovie = () => {
-    setshowMovie(true);
-  };
+    setshowMovie(true)
+  }
   const onMute = () => {
-    muted ? setMuted(false) : setMuted(true);
-  };
+    muted ? setMuted(false) : setMuted(true)
+  }
 
   return (
     <div className="smallMovieContainer">
-      {/* <button onClick={toggleMovie}  */}
-      {/* // className={` ${showMovie ? "smallDetailCard" : "hidden" }'} */}
-  
+      <button onClick={toggleMovie} />
+      <div className= {`${showMovie ? "smallDetailCard" : "hidden" }`} >
 
-      <ReactPlayer
-        url={`https://youtu.be/${props.movie?.youtubeKey}`}
-        playing={play}
-        muted={muted}
-        controls={false}
-        loop={true}
-        width="100%"
-        height="100%"
-      />
+        <ReactPlayer
+          url={`https://youtu.be/${props.movie?.youtubeKey}`}
+          playing={play}
+          muted={muted}
+          controls={false}
+          loop={true}
+          width="100%"
+          height="100%"
+        />
     </div>
-  );
+    </div>
+     )
 }
 
 export default RowMoreInfo;
