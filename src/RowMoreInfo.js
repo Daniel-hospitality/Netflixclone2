1e22ce071dad516ada7277248b3f8a140e01d7b7
Cin1983! <cinnymaria@gmail.com>
2021-02-18 10:59:37 +0100

    Update RowMoreInfo

--- src/RowMoreInfo.js
+++ src/RowMoreInfo.js
@@ -1,33 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
 import ReactPlayer from "react-player/youtube";
+// import LineIcon from "react-lineicons";
 
 function RowMoreInfo(props) {
-    const [showMovie, setshowMovie] = useState(false);
-    const [play, setPlay] = useState(true);
-    const [muted, setMuted] = useState(true);
-    const toggleMovie = () => {
-      setshowMovie(true);
-    }
+  const [showMovie, setshowMovie] = useState(false);
+  const [play, setPlay] = useState(true);
+  const [muted, setMuted] = useState(true);
+ 
 
-    return (
-<div>
-    <div className="playerWrapper" ref={playerWrapper}>
-    <ReactPlayer
-    url={`https://youtu.be/${props.movie?.youtubeKey}`}
-    playing={play}
-    muted={muted}
-    controls={false}
-    loop={true}
-    width="100%"
-    height="100%"
-     />
-    </div>
-    <div className="playbutton">
-    <button className="playButtonDetailCard" onClick={handleFullscreen}>
-    <i className="fas fa-caret-right" /> &nbsp;&nbsp; Play
-    </button>
-    </div>
-    )
+  
   
 
+  const toggleMovie = () => {
+    setshowMovie(true);
+  };
+  const onMute = () => {
+    muted ? setMuted(false) : setMuted(true);
+  }
+  
+
+  return (
+   
+    <div className="smallMovieContainer">
+         <div className={`${showMovie ? "smallDetailcard" : "hidden"}`}>
+      <button onClick={toggleMovie} />
+        <div className="icons-container">
+          <div className="plusIcon" id="icon-plus">
+            <i className="lni lni-plus"></i>
+          </div>
+          <div className="thumbsUp">
+            <i className="lni lni-thumbs-up"></i>
+          </div>
+          <div className="thumbsDown">
+            <i className="lni lni-thumbs-down"></i>
+          </div>
+          <div className="checkMark">
+          <i class="lni lni-checkmark"></i>
+          </div>
+        </div>
+        <ReactPlayer
+          url={`https://youtu.be/${props.movie?.youtubeKey}`}
+          playing={play}
+          muted={muted}
+          controls={false}
+          loop={true}
+          width="100%"
+          height="7.3rem"
+        />
+      </div>
+      </div>
+  
+    
+  );
+}
+
 export default RowMoreInfo;
+
