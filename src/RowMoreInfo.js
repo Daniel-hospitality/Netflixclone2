d2a99fb0e83e919e1768516a0ec4da24301c8845
Cin1983! <cinnymaria@gmail.com>
2021-02-20 00:41:53 +0100

    update row infobox

--- src/RowMoreInfo.js
+++ src/RowMoreInfo.js
@@ -1,29 +1,26 @@
 import React, { useState } from "react";
 import ReactPlayer from "react-player/youtube";
+import InfoButton from "./components/MoreInfoButton/InfoButton";
+import "../src/RowMoreInfo.css";
 // import LineIcon from "react-lineicons";
-
 function RowMoreInfo({ showMovie, movie }) {
   const [play, setPlay] = useState(true);
   const [muted, setMuted] = useState(true);
- ;
- 
 
+  // const [InfoButton, setInfoButton] = useState(false);
   // const moreInfo = ()=> {
   //   setsmallMovieContainer (true);
   // }
- 
-
- 
   const onMute = () => {
     muted ? setMuted(false) : setMuted(true);
-  }
-  
-
+  };
+  // const handleCheckMark = () => {
+  //   setInfoButton(true)
+  // }
   return (
-   
     <div className="smallMovieContainer">
-         <div className={`${showMovie ? "smallMovieContainer" : "hidden"}`}>
-      {/* <button onClick={toggleMovie} /> */}
+      <div className={`${showMovie ? "smallMovieContainer" : ""}`}>
+        {/* <button onClick={toggleMovie} /> */}
         <div className="icons-container">
           <div className="plusIcon" id="icon-plus">
             <i className="lni lni-plus"></i>
@@ -34,25 +31,24 @@ function RowMoreInfo({ showMovie, movie }) {
           <div className="thumbsDown">
             <i className="lni lni-thumbs-down"></i>
           </div>
-          <div className="checkMark">
+          {/* <div className="checkMark">
+          <InfoButton onClick={handleCheckMark} />
           <i class="lni lni-checkmark"></i>
-          </div>
+          </div> */}
         </div>
-        <ReactPlayer
-          url={`https://youtu.be/${movie?.youtubeKey}`}
-          playing={play}
-          muted={muted}
-          controls={false}
-          loop={true}
-          width="100%"
-          height="7.3rem"
-        />
+        <div className="movieWrapper">
+          <ReactPlayer
+            url="https://youtu.be/GDLv1x2bi4I"
+            playing={play}
+            muted={muted}
+            controls={false}
+            loop={true}
+            width="100%"
+            height="7.3rem"
+          />
+        </div>
       </div>
-      </div>
-  
-    
+    </div>
   );
 }
-
 export default RowMoreInfo;
-
