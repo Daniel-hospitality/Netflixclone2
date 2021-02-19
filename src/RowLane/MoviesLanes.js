4c09f9aa50c9877c1c50956714881f1b0ebe8255
Daniel <info@danielhospitality.nl>
2021-02-19 19:24:16 +0100

    testBack

--- src/RowLane/MoviesLanes.js
+++ src/RowLane/MoviesLanes.js
@@ -1,54 +1,71 @@
 import React, { useState, useRef, useEffect } from "react";
 import Backend from "../Backend";
-import "./RowLane.css";
-import Player from "../components/Player";
-
+import "./MoviesLanes.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
+function MoviesLanes({ title, genre, casts, isLargeRow }) {
+  const [movies, setMovies] = useState(null);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-function MoviesLanes({title, genre, casts, isLargeRow}) {
-    const [movies, setMovies] = useState(null);
-    const player = useRef(null);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await Backend.fetchGenres(genre);
+      //   const response = await Backend.fetchCast(casts);
 
-    useEffect(() => {
-        async function fetchData() {
-          const response = await Backend.fetchGenres(genre);
-        //   const response = await Backend.fetchCast(casts);
+      setMovies(response.data);
+    }
+    fetchData();
+  }, []);
 
-          setMovies(response.data);
-        }
-        fetchData();
-    }, []);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      //https://developers.google.com/youtube/player_parameters#?v=46646454546
+      autoplay: 0,
+    },
+  };
 
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
+  return (
+    <div className="movieslane">
+      <h2>{title}</h2>
+      <div className="movies_posters">
+        {movies?.map((movie) => {
+          return (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`movies_poster ${isLargeRow && "movies_posterLarge"}`}
+              src={
+                isLargeRow
+                  ? `${base_url}${movie.posterPath}`
+                  : movie.movieThumbUrl
+              }
+              alt={movie.title}
+            />
+          );
+        })}
+      </div>
 
-
-
-
-
-
-    return (
-        <div className="movieslane">
-            <h2>{title}</h2>
-            <div className="movies_posters">
-                {movies?.map(movie => {
-                    return <img
-                    key={movie.id}
-                    className={`movies_poster ${isLargeRow && "movies_posterLarge"}`}
-                    src={
-                        isLargeRow ? `${base_url}${movie.posterPath}` : movie.movieThumbUrl
-                    } alt={movie.name} />
-                })}
-            </div>
-            <div className="moviesplayer-wrapper">
-              <Player ref={player} movie={movies}/>
-            </div>
-            
-        </div>
-        
-    );
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
 }
 
 export default MoviesLanes;
-
