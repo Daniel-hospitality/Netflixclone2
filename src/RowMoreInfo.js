333ba1f8a650a622d1fd7295029a582804b3edd1
Cin1983! <cinnymaria@gmail.com>
2021-02-21 11:30:16 +0100

    Update styling

--- src/RowMoreInfo.js
+++ src/RowMoreInfo.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ReactPlayer from "react-player/youtube";
-import InfoButton from "./components/MoreInfoButton/InfoButton";
+// import InfoButton from "./components/MoreInfoButton/InfoButton";
 import "../src/RowMoreInfo.css";
 // import LineIcon from "react-lineicons";
 function RowMoreInfo({ showMovie, movie }) {
@@ -21,32 +21,28 @@ function RowMoreInfo({ showMovie, movie }) {
     <div className="smallMovieContainer">
       <div className={`${showMovie ? "smallMovieContainer" : ""}`}>
         {/* <button onClick={toggleMovie} /> */}
-        <div className="icons-container">
-          <div className="plusIcon" id="icon-plus">
-            <i className="lni lni-plus"></i>
-          </div>
-          <div className="thumbsUp">
-            <i className="lni lni-thumbs-up"></i>
-          </div>
-          <div className="thumbsDown">
-            <i className="lni lni-thumbs-down"></i>
-          </div>
-          {/* <div className="checkMark">
+        <div className="icons-container-small">
+          <i class="lni lni-play"></i>
+          <i className="lni lni-plus"></i>
+          <i className="lni lni-thumbs-up"></i>
+          <i className="lni lni-thumbs-down"></i>
+          <i class="lni lni-checkmark"></i>
+        </div>
+        {/* <div className="checkMark">
           <InfoButton onClick={handleCheckMark} />
           <i class="lni lni-checkmark"></i>
           </div> */}
-        </div>
-        <div className="movieWrapper">
-          <ReactPlayer
-            url="https://youtu.be/GDLv1x2bi4I"
-            playing={play}
-            muted={muted}
-            controls={false}
-            loop={true}
-            width="100%"
-            height="7.3rem"
-          />
-        </div>
+      </div>
+      <div className="movieWrapper">
+        <ReactPlayer
+          url=""
+          playing={play}
+          muted={muted}
+          controls={false}
+          loop={true}
+          width="100%"
+          height="7.3rem"
+        />
       </div>
     </div>
   );
