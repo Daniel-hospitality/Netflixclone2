75ef21d7fce99b2e2783337ea5bcdb6d2401c1d1
Abraham Engel <engelenburg1980@gmail.com>
2021-02-26 10:34:39 +0100

    refactor MovieLaneItem

--- src/RowLane/MovieLaneItem.js
+++ src/RowLane/MovieLaneItem.js
@@ -2,39 +2,39 @@ import React from "react";
 import "./MoviesLanes.css";
 import ReactPlayer from "react-player/youtube";
 
-
-function MovieLaneItem(props, info) {
-  console.log(info);
+function MovieLaneItem(props) {
+  const mystyle = {
+    position: "absolute",
+    left: props.posLeft,
+    top: props.posTop
+  };
   return (
-    <div className="movie_lane_itemc">
-      {/* <div className="playerWrapper" >
-              <ReactPlayer
-                url={`https://youtu.be/${props?.youtubeKey}`}
-                playing={true}
-                muted={true}
-                controls={false}
-                loop={true}
-                width="100%"
-                height="100%"
-                />
-               
-      </div> */}
-  
-      <div className="movies_title">
-        <h1>{props.title}</h1>
+    <div className="movie_lane_item" style={mystyle}>
+      <div className="movie-lane-player-wrapper">
+        <ReactPlayer
+          url={`https://youtu.be/${props.movie?.youtubeKey}`}
+          playing={true}
+          muted={true}
+          controls={false}
+          loop={true}
+          width="100%"
+          height="100%"
+          />
       </div>
-      <div className="movies_runtime">
-        <p>Runtime: {info.runtime}</p>
-      </div>{" "}
-      <div className="movies_director">
-        <p>Director: {info.director}</p>
-      </div>{" "}
+      <div className="movie-lane-item-btn-container">
+          <button className="banner_button">
+            <i className="fas fa-caret-right" /> &nbsp;&nbsp; Play
+          </button>
+          <button className="banner_button">
+            <i className="fas fa-info-circle"></i> &nbsp;&nbsp; Meer informatie
+          </button>
+      </div>
       <div className="movies_genres">
-        {/* <p>
-            {info.genres.map((id) => (
-              <li>{id.name}</li>
-            ))}
-          </p> */}
+        <p>
+          {props.movie?.genres.map((id) => (
+            <li>{id.name}</li>
+          ))}
+        </p>
       </div>
     </div>
   );
