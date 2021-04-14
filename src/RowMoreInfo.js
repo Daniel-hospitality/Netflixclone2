f09cf226ce81437443bab706ac29492330c305a0
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-04-14 19:41:56 +0200

    update

--- src/RowMoreInfo.js
+++ src/RowMoreInfo.js
@@ -1,45 +1,45 @@
-import React, { useState } from "react";
-import FilePlayer from "react-player/file";
-import ReactPlayer from "react-player/youtube";
-import Player from "./components/Player"
-// import InfoButton from "./components/MoreInfoButton/InfoButton";
-import "../src/RowMoreInfo.css";
-// import LineIcon from "react-lineicons";
-function RowMoreInfo({ showMovie, movie }) {
-  const [play, setPlay] = useState(true);
-  const [muted, setMuted] = useState(true);
+// import React, { useState } from "react";
+// // import FilePlayer from "react-player/file";
+// // import ReactPlayer from "react-player/youtube";
+// import Player from "./components/Player"
+// // import InfoButton from "./components/MoreInfoButton/InfoButton";
+// import "../src/RowMoreInfo.css";
+// // import LineIcon from "react-lineicons";
+// function RowMoreInfo({ showMovie, movie }) {
+//   // const [play, setPlay] = useState(true);
+//   const [muted, setMuted] = useState(true);
 
-  // const [InfoButton, setInfoButton] = useState(false);
-  // const moreInfo = ()=> {
-  //   setsmallMovieContainer (true);
-  // }
-  const onMute = () => {
-    muted ? setMuted(false) : setMuted(true);
-  };
-  // const handleCheckMark = () => {
-  //   setInfoButton(true)
-  // }
-  return (
-    <div className="smallMovieContainer">
-      <div className={`${showMovie ? "smallMovieContainer" :"hidden"}`}>
-        {/* <button onClick={toggleMovie} /> */}
-        <div className="icons-container-small">
-          <i class="lni lni-play" id="play"></i>
-          <i className="lni lni-plus" id="plus-icon"></i>
-          <i className="lni lni-thumbs-up" id="thumbs-up"></i>
-          <i className="lni lni-thumbs-down" id="thumbs-down"></i>
-          <i class="lni lni-checkmark" id="checkmark-icon"></i>
-        </div>
+//   // const [InfoButton, setInfoButton] = useState(false);
+//   // const moreInfo = ()=> {
+//   //   setsmallMovieContainer (true);
+//   // }
+//   // const onMute = () => {
+//     muted ? setMuted(false) : setMuted(true);
+//   };
+//   // const handleCheckMark = () => {
+//   //   setInfoButton(true)
+//   // }
+//   return (
+//     <div className="smallMovieContainer">
+//       <div className={`${showMovie ? "smallMovieContainer" :"hidden"}`}>
+//         {/* <button onClick={toggleMovie} /> */}
+//         <div className="icons-container-small">
+//           <i class="lni lni-play" id="play"></i>
+//           <i className="lni lni-plus" id="plus-icon"></i>
+//           <i className="lni lni-thumbs-up" id="thumbs-up"></i>
+//           <i className="lni lni-thumbs-down" id="thumbs-down"></i>
+//           <i class="lni lni-checkmark" id="checkmark-icon"></i>
+//         </div>
 
-        {/* <div className="checkMark">
-          <InfoButton onClick={handleCheckMark} />
-          <i class="lni lni-checkmark"></i>
-          </div> */}
-      </div>
-      <div className="movieWrapper">
-        <Player />
-      </div>
-    </div>
-  );
-}
-export default RowMoreInfo;
+//         {/* <div className="checkMark">
+//           <InfoButton onClick={handleCheckMark} />
+//           <i class="lni lni-checkmark"></i>
+//           </div> */}
+//       </div>
+//       <div className="movieWrapper">
+//         <Player />
+//       </div>
+//     </div>
+//   );
+// }
+// export default RowMoreInfo;
